c0cdf141a33d7cf555abbfca1e6cf13f5f08a57a
katanabe <nabeon+github@gmail.com>
2025-08-26 13:46:26 +0900

    fix: ESLint エラーを修正
    - any 型使用箇所に eslint-disable コメントを追加
    - data パラメータを正しく使用するように修正
    - TODO コメントで型定義の改善が必要であることを記載

--- apps/frontend/src/lib/api-client.ts
+++ apps/frontend/src/lib/api-client.ts
@@ -9,9 +9,12 @@ export const api = {
     getById: (id: string) => client.api.todos[':id'].$get({ param: { id } }),
     create: (data: { title: string; description?: string }) => 
       client.api.todos.$post({ json: data }),
+    // TODO: Fix type definition for PUT endpoint with json body
     update: (id: string, data: { title?: string; description?: string; completed?: boolean }) => 
       client.api.todos[':id'].$put({ 
-        param: { id }
+        param: { id },
+        json: data
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } as any),
     delete: (id: string) => 
       client.api.todos[':id'].$delete({ param: { id } }),
